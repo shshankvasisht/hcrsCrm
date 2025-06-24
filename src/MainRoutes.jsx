378c791589a273b9df7c78ab1172e7b9569b5bdf
shshankvasisht <vasishtshshank@gmail.com>
2025-06-24 12:27:36 +0530

    routing issue fixed

--- src/MainRoutes.jsx
+++ src/MainRoutes.jsx
@@ -1,17 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Users from './pages/Users';
-import AdminLayout from './layouts/AdminLayout';
-import Profiles from './pages/Profiles';
-import Documents from './pages/Documents';
-import UserLayout from './layouts/UserLayout';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';;
 import ProtectedRoutes from './helpers/ProtectedRoutes';
 
 const MainRoutes = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
     return (
         <Routes>
-            <Route path="/" element={<Login />} />
+            <Route
+                path="/"
+                element={
+                    user ? (
+                        user.user_type === '2' ? (
+                            <Navigate to="/admin/dashboard" replace />
+                        ) : (
+                            <Navigate to="/user/dashboard" replace />
+                        )
+                    ) : (
+                        <Login />
+                    )
+                }
+            />
             <Route path="/*" element={<ProtectedRoutes />} />
         </Routes>
     );
